e3b2455d1da209d11308483d17bfd9631c549b0e
Milosz Wach <wach.milosz1@gmail.com>
2025-06-04 16:23:20 +0200

    .

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Headers";
+import Quiz from "./components/Quiz";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Quiz />
+      </main>
+    </>
+  )
+}
+export default App;
